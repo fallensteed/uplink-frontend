066d22d3f49bf8d5cd21cb8726aad7177554a656
Scott Hubert <scott.hubert.1@spaceforce.mil>
2022-11-02 16:29:15 -0600

    SH: Started initial sytling and themes.

---
+++ src/common/config/theme.tsx
@@ -0,0 +1,22 @@
+import { PaletteMode } from "@mui/material"
+
+const fontFamily = [
+    "Noto Sans",
+    "Roboto",
+    "Spartan",
+    "-apple-system",
+    "Segoe UI",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "Fira Sans",
+    "Droid Sans",
+    "Helvetica Neue",
+    "Helvetica",
+    "Montserrat",
+    "Arial",
+    "sans-serif",
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+].join(",");
